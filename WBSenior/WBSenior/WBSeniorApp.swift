85f13ace4b2cc17a695047b6149716cdcea43e04
Плетников Александр Александрович <pletnikov.a@wb.ru>
2024-07-17 09:37:42 +0300

    create project

---
+++ WBSenior/WBSenior/WBSeniorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSeniorApp.swift
+//  WBSenior
+//
+//  Created by Александр Плетников on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBSeniorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
